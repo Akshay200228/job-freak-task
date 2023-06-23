e759da0e005a150f85729151075eaf8d20cb237e
Akshay200228 <sankpla2812@gmail.com>
2023-06-23 15:55:23 +0530

    initial commet

---
+++ script.js
@@ -0,0 +1,15 @@
+const showContent = (contentId) => {
+    let content = document.getElementById(contentId);
+    let allContent = document.getElementsByClassName('content')[0].children;
+
+    // Hide all content
+    for (let i = 0; i < allContent.length; i++) {
+        allContent[i].style.display = 'none';
+    }
+
+    // Show selected content
+    content.style.display = 'block';
+}
+
+// Set default content to 'Home'
+document.getElementById('home').style.display = 'block';
